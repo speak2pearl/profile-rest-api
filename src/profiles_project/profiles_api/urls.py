08d49947d8f1baa5095d204711b25c98ddfa28d8
Jyoti K <speak2pearl@gmail.com>
2020-04-03 12:53:06 +0530

    Added rest view and hello view APIs

---
+++ src/profiles_project/profiles_api/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . from views
+
+urlpatterns = [
+    url(r'^hello-view/',views.HelloApiView.as_view())
+]
+
+# This will also work
+# from django.conf.urls import url
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('hello-view/',views.HelloApiView.as_view())
+# ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View """
+
+    def get(self,request,format=None):
+
+        an_apiview = [
+            'Uses HTTP method as functions - get, post, patch, put, delete'
+            'It is similar to traditional django view'
+            'Gives you the most control over you logic'
+            'It mapped manually to the URLs'
+        ]
+
+        return Response({'message':'Hello',  'an_apiview': an_apiview})
 
 # Create your views here.
